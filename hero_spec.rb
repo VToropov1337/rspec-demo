db5f3c8d86a56c54d07b238d868aa96681ecc876
VToropov1337 <vladimirtoropov87@gmail.com>
2017-03-09 15:11:40 +0300

    add hero test

--- hero_spec.rb
+++ hero_spec.rb
@@ -2,5 +2,29 @@ require './hero'
 
 
 describe Hero do
+
+	it "has a capitalized name" do
+		hero = Hero.new 'Mike'
+
+		expect(hero.name).to eq 'Mike'   # hero.name == 'Mike'
+	end
+
+	it "can power up" do 
+		hero = Hero.new 'Mike'
+
+		expect(hero.power_up).to eq 110
+	end
 	
+	it "can power down" do
+		hero = Hero.new 'Mike'
+
+		expect(hero.power_down).to eq 90
+	end
+
+	it "displays full hero info" do
+		hero = Hero.new 'Mike'
+
+		expect(hero.hero_info).to eq 'Mike has a health 100'
+	end
+
 end
